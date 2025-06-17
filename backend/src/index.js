0f5bbb4d8871b9b8f27b24e43b1071694cbeb6ea
Dasso Ahmed <dassoahmed@gmail.com>
2025-06-17 10:23:51 +0100

    get recommended books Update bookRoutes.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 import authRoutes from "./routes/authRoutes.js";
@@ -7,10 +8,12 @@ import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 // Add this middleware before your routes
 app.use(express.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -67,22 +67,32 @@ router.get("/", protectRoute, async (req, res) => {
     }
 })
 
+//get recommended books by logged in user
+router.get("/user", protectRoute, async (req, res) => {
+    try {
+        // Fetch books created by the logged-in user
+        const Books = await Book.find({ user: req.user._id }).sort({ createdAt: -1 }); // Sort by creation date
+        res.json(Books);
+    } catch (error) {
+        console.error("Error fetching recommended books:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
 //delete a book
 router.delete("/:id", protectRoute, async (req, res) => {
     try {
         const bookId = req.params.id;
         // Find the book by ID and delete it
         const Book = await Book.findByIdAndDelete(bookId);
-
         if (!Book) return res.status(404).json({ message: "Book not found" });     
-        res.status(200).json({ message: "Book deleted successfully" });
 
         //check if user is the creator of the book
         if (Book.userId.toString() !== req.user._id.toString()); 
         return res.status(403).json({ message: "You are not authorized to delete this book" });
         
         // Optionally, you can also delete the image from Cloudinary if needed
-        if(Book.image && Book.imageincludes("cloudinary")) {
+        if(Book.image && Book.image.includes("cloudinary")) {
             try {
                const publicId = Book.image.split("/").pop().split(".")[0]; // Extract public ID from URL
                await cloudinary.uploader.destroy(publicId);     
@@ -96,7 +106,7 @@ router.delete("/:id", protectRoute, async (req, res) => {
         res.json({ message: "Book deleted successfully" });
     } catch (error) {
         console.error("Error deleting book:", error);
-        res.status(500).json({ message: error.message });
+        res.status(500).json({ message: "Internal server error" });
     }
 });
 
