3afef42ee2d6da4fac9d196cc56e849818f1ecf4
Dasso Ahmed <dassoahmed@gmail.com>
2025-07-02 11:21:25 +0100

    rending items home UI index.jsx

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -40,15 +40,15 @@ router.post("/", protectRoute, async (req, res) => {
 
 //pagination => infinite Scroll
 router.get("/", protectRoute, async (req, res) => {
- // exa;ple call from react native - frontend
+ // example call from react native - frontend
  // const response = await fetch ("http://localhost:3000/api/books?page=1&limit=5");
     try {
         const page = req.query.page || 1;
-        const limit = req.query.limit || 5;
+        const limit = req.query.limit || 2;
         const skip = (page - 1) * limit;
 
         const books = await Book.find()
-        .sort({ createdAt: -1})// desc
+        .sort({ createdAt: -1 })// desc
         .skip(skip)
         .limit(limit)
         .populate("user", "username profileImage");
